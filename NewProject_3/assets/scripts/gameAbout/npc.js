fe5d0f6afef4f198d00f45bc62538f02e106ebc6
2855239858 <2855239858@qq.com>
2019-04-22 18:55:23 +0800

    脚本替换

--- NewProject_3/assets/scripts/gameAbout/npc.js
+++ NewProject_3/assets/scripts/gameAbout/npc.js
@@ -6,16 +6,27 @@ cc.Class({
 
     properties: {
         drumer: cc.Node,
-        xSpeed:0,
-        randomSpeed:0,
+        xSpeed: 0,
+        randomSpeed: 0,
     },
 
     start() {
-        
+        var a = this.npcJudge();
+        switch(a){
+            case 1:
+            this.randomSpeed = this.randomFrom(60,80);
+            break;
+            case 2:
+            this.randomSpeed = this.randomFrom(100,120);
+            break;
+            case 3:
+            this.randomSpeed = this.randomFrom(150,170);
+            break;
+        }
+
     },
 
-    onLoad: function(){
-        this.randomSpeed = this.randomFrom(50,150);
+    onLoad: function () {
     },
 
     // onLoad: function () {
@@ -25,6 +36,21 @@ cc.Class({
     //     this.node.runAction(npcMove);
     // },
 
+    npcJudge() {
+        if (this.node.name == 'npc1') {
+            console.log('this is npc1');
+            return 1;
+        } else if (this.node.name == 'npc2') {
+            console.log('this is npc2');
+            return 2;
+        } else if (this.node.name == 'npc3') {
+            console.log("this is npc3");
+            return 3;
+        }else{
+            console.log('Wrong');
+        }
+    },
+
     randomFrom(lowerValue, upperValue) {
         return Math.floor(Math.random() * (upperValue - lowerValue + 1) + lowerValue);
     },
@@ -32,7 +58,7 @@ cc.Class({
     update: function (dt) {
         if (this.node.x < ENDLINE) {
             this.xSpeed = this.randomSpeed;
-        }else{
+        } else {
             this.xSpeed = 0
         }
         this.node.x += this.xSpeed * dt;
